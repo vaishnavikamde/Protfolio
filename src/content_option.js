fe42d10435ba3f8cf27943504f2e4fe872e62fef
Vaishnavi Kamde <vaishnavikamde02@gmail.com>
2024-03-27 01:33:35 +0530

    portfolio
    My Personal Portfolio

---
+++ src/content_option.js
@@ -0,0 +1,138 @@
+const logotext = "Vaishnavi";
+const meta = {
+    title: "Vaishnavi kamde",
+    description: "I’m Vaishnavi Kamde data scientist _ Front-End devloper",
+};
+
+const introdata = {
+    title: "I’m Vaishnavi Kamde",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop mobile apps",
+    },
+    description: "I'm skilled and versatile developer capable of handling all aspects of web application development, from design to deployment. ",
+    your_img_url: "https://images.unsplash.com/photo-1544005313-94ddf0286df2?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cGVyc29ufGVufDB8fDB8fHww",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "As a Front-End developer, I am capable of developing both the front-end components of a web application. I have understanding of web development technologies and programming languages such as HTML, CSS, JavaScript, ReactJS and databases like MySQL. I have experience with web development frameworks such as React . I am proficient in using tools like Git. I am able to create a complete web application from scratch, starting with designing the user interface, developing logic, and integrating the front-end components into a seamless application.!",
+};
+const worktimeline = [
+    {
+        jobtitle: "Testing",
+        where: "Infycomp",
+        date: "2024",
+    },
+    {
+        jobtitle: "Front-End developer",
+        where: "Pune",
+        date: "2023",
+    },
+
+    {
+        jobtitle: "College Projects",
+        where: "College",
+        date: "2022",
+    },
+];
+
+const skills = [{
+        name: "HTML",
+        value: 90,
+    },
+    {
+        name: "CSS",
+        value: 85,
+    },
+    {
+        name: "JavaScript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 80,
+    },
+    {
+        name: "Bootstrap",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Wordpress Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://plus.unsplash.com/premium_photo-1664297902473-4b5f8bc8ae70?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://images.unsplash.com/photo-1580927752452-89d86da3fa0a?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://images.unsplash.com/photo-1504639725590-34d0984388bd?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://images.unsplash.com/photo-1580894908361-967195033215?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://images.unsplash.com/photo-1530020722985-452e278c5114?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    
+    {
+        img: "https://plus.unsplash.com/premium_photo-1706548332644-2600407c133d?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "vaishnavikamde02@gmail.com",
+    YOUR_FONE: 7828865472,
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/vaishnavikamde",
+    facebook: "https://facebook.com",
+    linkedin: "https://www.linkedin.com/in/vaishnavikamde24/",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
